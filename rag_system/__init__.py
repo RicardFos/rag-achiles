5cf14badbaa7f3367a27ad5c49622458256ad7b6
Ricard Fos <ricard.fos@idiada.com>
2026-06-04 09:04:21 +0200

    initial commit

---
+++ rag_system/__init__.py
@@ -0,0 +1,9 @@
+"""
+RAG System for Document Q&A
+Achiles Interview Assignment
+
+A complete RAG pipeline for ingesting PDFs, generating embeddings,
+and answering questions with citations.
+"""
+
+__version__ = "1.0.0"
